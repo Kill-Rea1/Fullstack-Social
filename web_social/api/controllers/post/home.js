e718ae5aa9d02c05f78adea7ed4a31489c0eb6d7
Kirill Ivanov <ivanofff.k@gmail.com>
2019-10-27 19:04:16 +0300

    Custom HTML/CSS Login page

--- web_social/api/controllers/post/home.js
+++ web_social/api/controllers/post/home.js
@@ -2,10 +2,11 @@ module.exports = async function(req, res) {
     // console.log("Show the post creation form now")
     sails.log.info("Show the post creation form now")
 
-    await Post.destroy({})
+    // await Post.destroy({})
 
     const userId = req.session.userId
-    const allPosts = await Post.find({user: userId}).populate('user')
+    const allPosts = await Post.find({user: userId})
+        .populate('user')
         .sort('createdAt DESC')
 
     res.view('pages/post/home', {
--- web_social/config/routes.js
+++ web_social/config/routes.js
@@ -57,6 +57,8 @@ module.exports.routes = {
   'PUT   /api/v1/account/update-profile':             { action: 'account/update-profile' },
   'PUT   /api/v1/account/update-billing-card':        { action: 'account/update-billing-card' },
   'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
+  // Solution 1. PUT -> POST (Copy)
+  // 'POST   /api/v1/entrance/login':                        { action: 'entrance/login' },
   'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
   'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
   'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
@@ -65,5 +67,11 @@ module.exports.routes = {
   'GET /listusers': { action: 'user/listusers' },
   'GET /post': { action: 'post/home' },
   'POST /post': {action: 'post/create' },
+  'GET /customauth/login': {
+    view: 'pages/customauth/custom-login',
+    locals: {
+      layout: 'layouts/auth-layout'
+    }
+  }
 
 };
